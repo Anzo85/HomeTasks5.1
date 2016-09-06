75a965ca943122adb754eafb2af484530c9f1137
Anzo85 <zolotarev@3g.ua>
2016-09-06 16:11:25 +0300

    add return

--- src/main/java/org/anzo/SumArray.java
+++ src/main/java/org/anzo/SumArray.java
@@ -9,14 +9,11 @@ import java.util.Arrays;
 public class SumArray {
 
 
-    public static void summary(int[] array1, int[] array2) {
+    public static int[] summary(int[] array1, int[] array2) {
 
         int[] sumArray = ArrayUtils.addAll(array1, array2);
         Arrays.sort(sumArray);
 
-        for (int i = 0; i < sumArray.length; i++) {
-            System.out.println(sumArray[i]);
-        }
-
+       return  sumArray ;
     }
 }
